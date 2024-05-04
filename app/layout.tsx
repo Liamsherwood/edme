afcc750f4ac607301090390b52d1e4cc728aa191
Liam Sherwood <liam@thesherwoods.de>
2024-05-04 20:19:45 +0200

    site info

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import SplashScreenWrapper from "@/components/global/splashscreenwrapper";
 
 
 export const metadata: Metadata = {
-    title: SITE.title || "edme.",
+    title: SITE.title || "Edme",
     description: SITE.desc || "Your company description.",
     keywords: SITE.keywords || "Your company keywords",
     icons: {
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
         }
     },
     openGraph: {
-        title: SITE.title || "Your Title",
+        title: SITE.title || "Edme",
         description: SITE.desc || "Your company description.",
         authors: SITE.author || "Your Author",
         images: [
--- config.ts
+++ config.ts
@@ -5,9 +5,9 @@ import { Site } from "./types";
 export const SITE: Site = {
     siteUrl: "https://www.edme.vercel.app/", // Always put "/" at the end of the URL
     author: "Liam Sherwood",
-    desc: "Welcome to Bent-o, A bento theme made for you!",
-    title: "edme.",
-    ogImage: "https://www.chrstnl.com/oggraph-image.png",
+    desc: "Unlocking human potential through accessible learning.",
+    title: "Edme",
+    ogImage: "/public/OGimage.png",
     keywords:
-        "NextJS, NextJS Templates, TailwindCSS, Premium NextJS and Tailwind CSS Templates, NextJS and TailwindCSS Templates, NextJS Boilerplate template, NextJS Boilerplate, Premium NextJS and TailwindCSS Templates, Premium NextJS and Tailwind CSS Themes, NextJS and Tailwind CSS Boilerplate, Premium NextJS and Tailwind CSS themes, nextjs themes, tailwind templates, NextJS themes, Personal portfolio, landing page, page template, developer portfolio, designer portfolio"
+        "Edme, Edme Education, Edme Blog, Educational Social Media, Social Microlearning Platform, Edme Platform, Accessible Learning, Edme Mission, Edme Mission and Vision, Edme Philosophy, About Edme, Edme Newsletter, Edme Website, Educate Me, Edme Founders, Education Startup, Tiktok for Learning, Educational Linkedin, Future of Education, Bite-sized learning, on-demand education, learn on the go, improve my skills, learn something new, learn something today, microlearning, new social media platform, educational social media platform, educational social media app, Berlin education startup, Berlin education app, Berlin education platform, Berlin educational social media, Berlin educational social media platform, Berlin educational social media app, Berlin educational social media startup, Berlin educational social media platform startup, Berlin educational social media app startup, Berlin educational social media startup, Berlin educational social media platform startup, Berlin educational social media app startup, Berlin educational social media startup, Social Media für Bildung, Edme Bildungs App, Edme Bildungs Plattform, Edme Bildungs Social Media, Edme Bildungs Social Media Plattform, Edme Bildungs Social Media App, Berlin Bildungs Startup, Berlin Bildungs App, Berlin Bildungs Plattform, Berlin Bildungs Social Media, Berlin Bildungs Social Media Plattform, Berlin Bildungs Social Media App, Berlin Bildungs Social Media Startup, Berlin Bildungs Social Media Plattform Startup, Berlin Bildungs Social Media App Startup, Berlin Bildungs Social Media Startup",
 };
